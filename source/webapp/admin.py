a5f229e44a229904062a2c02130f1c6b111d8217
mtashtanovski <mtashtanovski@gmail.com>
2022-01-08 12:55:19 +0600

    Created new model of Guest in models.py and registered it in admin.py. Made first initial migration. Added some data of guests from admin panel and dumped this data.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from webapp.models import Guest
+
+
 # Register your models here.
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['id', 'guest_name', 'guest_mail', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['guest_name']
+    fields = [
+        'guest_name',
+        'guest_mail',
+        'guest_text',
+        'created_at',
+        'updated_at',
+        'status'
+    ]
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Guest, GuestAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+STATUS_CHOICES = [('active', 'Active'), ('blocked', 'Blocked')]
+
+
+class Guest(models.Model):
+    guest_name = models.CharField(max_length=40, null=False, blank=False, verbose_name='Name')
+    guest_mail = models.EmailField(max_length=254, null=False, blank=False, verbose_name='E-mail')
+    guest_text = models.TextField(max_length=2000, null=False, blank=False, verbose_name='Text')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created date")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated date")
+    status = models.CharField(max_length=20, default='active', choices=STATUS_CHOICES, verbose_name='Status')
+
+    def __str__(self):
+        return f"{self.pk}. {self.guest_name}, {self.guest_mail}. {self.guest_text}"
+
+    class Meta:
+        db_table = 'guestbook'
+        verbose_name = 'Guest'
+        verbose_name_plural = 'Guests'
