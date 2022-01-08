1a90c9ce587ae7cbb62f30c7916db47de8fe36d8
mtashtanovski <mtashtanovski@gmail.com>
2022-01-08 13:56:02 +0600

    Modified view of index.html inherited by created new base.html. In main page notes are ordered by descending (the newest first). Added some bootstrap styles.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
-
 # Create your views here.
+from webapp.models import Guest, STATUS_CHOICES
+
+
 def index_view(request):
-    return render(request, 'index.html')
+    guests = Guest.objects.order_by('-created_at')
+    return render(request, 'index.html', {'guests': guests})
